9ae22c0f433bfb23c727e4cd5e21c10f116b2e0c
Atanas Minkov <atanas.minkov@icloud.com>
2020-04-19 15:28:21 +0300

    minor refactor; add "new article" feature; add json response option to GET /article/:id

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,9 +2,34 @@ class ArticlesController < ApplicationController
   def show
     article_id = params[:id]
     @article = Article.find(article_id)
+    format = params[:format]
+    if format == "json"
+      render json: @article
+    else
+      render "articles/show"
+    end
   end
 
   def index
     @articles = Article.all
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(permit_article_fields)
+    if @article.save
+      redirect_to @article
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def permit_article_fields
+    params.require(:article).permit(:title, :description)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'home' => 'pages#home'
-  get 'about' => 'pages#about'
-  resources :articles, only: [:show, :index]
-  root 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "home" => "pages#home"
+  get "about" => "pages#about"
+  resources :articles, only: %i[show index new create]
+  root "pages#home"
 end
